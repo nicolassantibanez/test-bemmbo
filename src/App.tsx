4c5ee94877ac7de289dfc73f110e47bc8763fafe
rinorambuena <nora@bemmbo.com>
2025-06-17 16:45:26 -0400

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import bemmboLogo from './assets/bemmbo-logo.svg'
+import bemmboLogo from "./assets/bemmbo-logo.svg";
 
 function App() {
   return (
     <>
-     {/* Build your page here */}
+      {/* Build your page here */}
       <div className="flex justify-center items-center h-screen w-screen bg-slate-50">
         <div className="flex flex-col items-center justify-center gap-4">
           <img src={bemmboLogo} alt="Bemmbo Logo" className="w-1/2 h-1/2" />
-          <h1 className="text-3xl font-bold text-blue-600">Prueba técnica Bemmbo</h1>
+          <h1 className="text-3xl font-bold text-blue-600">
+            Prueba técnica Bemmbo
+          </h1>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
